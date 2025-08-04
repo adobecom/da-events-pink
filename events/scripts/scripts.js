4d696345a28223e67a10ced354370a0de8ae23bc
fg-repo-sync <fg-repo-sync[bot]@users.noreply.github.com>
2025-08-04 08:31:26 +0000

    Syncing da-events to da-events-pink

--- events/scripts/scripts.js
+++ events/scripts/scripts.js
@@ -275,15 +275,17 @@ if (getMetadata('event-details-page') === 'yes') await validatePageAndRedirect(L
   });
 }());
 
-(async function loadPage() {
+async function loadPage() {
   await loadLana({ clientId: 'events-milo' });
   await loadArea().then(() => {
     if (getMetadata('event-details-page') === 'yes') lazyCaptureProfile();
   });
-}());
+}
 
 (async function loadDa() {
   if (!new URL(window.location.href).searchParams.get('dapreview')) return;
   // eslint-disable-next-line import/no-unresolved
   import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
 }());
+
+loadPage();
